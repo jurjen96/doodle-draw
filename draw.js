67a438b80f863184af75a47cc8ff2e867a9ee38c
Jurjen <jurjen.brouwer@ziggo.nl>
2019-11-18 17:26:20 +0100

    Added a better trained model and updated the UI a bit

--- draw.js
+++ draw.js
@@ -1,15 +1,22 @@
-var canvas = document.getElementById('canvas')
+const labels = [
+  'car', 'chair', 'bird', 'broom', 'butterfly', 'candle', 'clock',
+  'flashlight', 'flower', 'airplane', 'house', 'violin', 'sock' ]
+
+var canvas = document.getElementById('doodle-canvas')
 var context = canvas.getContext('2d')
 context.fillStyle = '#FFF'
 context.lineCap = 'round'
 context.fillRect(0, 0, canvas.width, canvas.height);
 context.strokeStyle = 'black'
-context.lineWidth = 8
+context.lineWidth = 12
 
 let x = 0, y = 0
 let isMouseDown = false
 
-const stopDrawing = () => isMouseDown = false
+const stopDrawing = () => {
+  isMouseDown = false
+  run()
+}
 const startDrawing = event => {
     isMouseDown = true;
    [x, y] = [event.offsetX, event.offsetY]
@@ -29,23 +36,43 @@ const drawLine = event => {
 canvas.addEventListener('mousedown', startDrawing)
 canvas.addEventListener('mousemove', drawLine)
 canvas.addEventListener('mouseup', stopDrawing)
-canvas.addEventListener('mouseout', stopDrawing)
+// canvas.addEventListener('mouseout', stopDrawing)
 
 run = async () => {
   // Get the CanvasPixelArray from the given coordinates and dimensions.
-  let imgData = context.getImageData(0,0,canvas.width,canvas.height) //.data
-  let norm = tf.scalar(255.0)
-  let tensor = tf.browser.fromPixels(imgData, 1)
-                  .resizeNearestNeighbor([28,28])
-                  .toFloat()
-                  .div(norm)
-                  .expandDims()
+  let imgData = context.getImageData(0,0,canvas.width,canvas.height)
 
-  const model = await tf.loadLayersModel('https://jurjen96.github.io/doodle-draw/tfjs_model/model.json')
-  console.log(model)
-  model.predict(tensor).data().then(predictions => {
-    console.log(predictions)
-  })
+  let norm = tf.scalar(255.0)
+  let invert = tf.scalar(1.0)
+  let tensor = tf.browser.fromPixels(imgData, 1)
+                  .resizeBilinear([28,28])
+                  // .resizeNearestNeighbor([28,28])
+                  .toFloat()
+                  .div(norm) // Normalize the data from 255 max to 1.0
+                  // .invert()
+  tensor = invert.sub(tensor) // Invert the colors
+  tensor.print()
+  console.log(tensor.dataSync())
+  // tf.browser.toPixels(tensor, document.getElementById("canvas2"))
+  tensor = tensor.toFloat()
+                 .expandDims()
+
+
+  const model = await tf.loadLayersModel('https://jurjen96.github.io/doodle-draw/object_model/model.json')
+  let predictions = await model.predict(tensor).data()
+  console.log(predictions)
+  let predicted = await tf.argMax(predictions).data()
+  console.log(predicted)
+  console.log(labels[predicted])
+  $("#prediction").html(labels[predicted])
 }
 
-run()
+$("#clear").click(() => {
+  console.log("Clearing")
+  context.fillRect(0, 0, canvas.width, canvas.height);
+})
+
+$("#next").click(() => {
+  console.log("Guessing")
+  run()
+})
--- main.py
+++ main.py
@@ -8,51 +8,60 @@ from keras.optimizers import SGD
 from keras.utils import np_utils
 
 from sklearn.model_selection import train_test_split
-# import tensorflowjs as tfjs
 import matplotlib.pyplot as plt
 
 from tqdm import tqdm
-
-data = []
-data.append([np.load('resources/numpy_bitmap/car.npy'), 'car'])
-data.append([np.load('resources/numpy_bitmap/bed.npy'), 'bed'])
-data.append([np.load('resources/numpy_bitmap/apple.npy'), 'apple'])
-
-#plt.imshow(X=data[3].reshape((28,28)))
-#plt.show()
 
 REBUILD_DATA = True
 
 LABELS = {
     'car': 0,
-    'apple': 1,
-    'bed': 2
+    'chair': 1,
+    'bird': 2,
+    'broom': 3,
+    'butterfly': 4,
+    'candle': 5,
+    'clock': 6,
+    'flashlight': 7,
+    'flower': 8,
+    'airplane': 9,
+    'house': 10,
+    'violin': 11,
+    'sock': 12
 }
 
 def create_training_data():
     features = []
     labels = []
-    for doodle in tqdm(data):
-        for img in doodle[0]: #[:200]:
-            features.append(img) #.reshape(28,28)
+    dataset = []
+
+    print("Loading the dataset")
+    for label in tqdm(LABELS):
+        dataset.append([np.load('resources/numpy_bitmap/' + label + '.npy'), label])
+
+    print("Appending it to the labels and features")
+    for doodle in tqdm(dataset):
+        for img in doodle[0][:50000]:
+            features.append(img)
             labels.append(np.eye(len(LABELS))[LABELS[doodle[1]]])
-            # training_data.append([img.reshape((28,28)), np.eye(2)[LABELS[doodle[1]]]])  #img 1 label
     # np.random.shuffle(training_data)
-    # np.save("car_bed.npy", training_data)
+    print("")
+    np.save('object_features', np.array(features))
+    np.save('object_labels', np.array(labels))
     return features, labels
 
 if REBUILD_DATA:
     data, labels = create_training_data()
 else:
-    training_data = np.load('car_bed.npy', allow_pickle=True)
+    data = np.load('object_features.npy', allow_pickle=True)
+    labels = np.load('object_labels.npy', allow_pickle=True)
 
 data = np.array(data)
 
+
+
 # Normalize the data
 data = data / 255
-
-X = data
-y = labels
 
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.33)
 
@@ -62,57 +71,58 @@ X_test = X_test.reshape(-1, 28, 28, 1)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
 
+print(y_train[0])
+plt.imshow(X=X_train[0].reshape((28,28)))
+plt.show()
+
+
+print(y_train[1])
+plt.imshow(X=X_train[1].reshape((28,28)))
+plt.show()
+
+# To save some memory:
+del data
+del labels
+
 model = Sequential()
-# model.add(Conv2D(64, (3,3), input_shape=(28, 28, 1)))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2,2)))
-# model.add(Conv2D(64, (3,3)))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2,2)))
-# model.add(Flatten())
-# model.add(Dense(64))
-# model.add(Dense(2))
-# model.add(Activation('softmax'))
-# model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
-#
-# model.fit(X_train, y_train, batch_size=64, epochs=10)
-
-
 
 model.add(Conv2D(64, (3, 3), input_shape=(28, 28, 1)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.3))
 model.add(Conv2D(128, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.4))
 
 model.add(Flatten()) # Convert the 3D features (height, width, features) to 1D feature map
-model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(3, activation='softmax'))
+model.add(Dense(len(LABELS), activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
 history = model.fit(X_train, y_train,
-                    epochs=10,
-                    batch_size=64,
+                    epochs=100,
+                    batch_size=256,
                     validation_split=0.2)
 
-test_loss, test_acc = model.evaluate(X_test, y_test, batch_size=32)
+test_loss, test_acc = model.evaluate(X_test, y_test, batch_size=256)
 
 print("SCORE:")
 print("Test loss: ", test_loss)
 print("Test accuracy", test_acc)
 
 
-print(history.history)
-print(history.history.keys())
+print("Saving model...")
+model.save('cnn_object_model_100_epoch_256_batch_' + str(time.time()) + '.h5')
+
 # Plot training & validation accuracy values
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
@@ -121,7 +131,7 @@ plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
-#
+
 # Plot training & validation loss values
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -131,7 +141,10 @@ plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 
+# 50 epochs
+# Test loss:  0.227007402865481
+# Test accuracy 0.9346247315406799
 
-print("Saving model...")
-model.save('cnn_model_3_' + str(time.time()) + '.h5')
-# tfjs.converters.save_keras_model(model, "model.json")
+# 100 epochs
+# Test loss:  0.22372005734438108
+# Test accuracy 0.9357156157493591
